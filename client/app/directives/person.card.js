989bd4ba4e80cc6ed2f4afa2b2b14a6c76b29276
Davi Alves <hey@davialves.com>
2015-11-22 14:57:50 -0300

    card delete btn and feedback

--- client/app/directives/person.card.js
+++ client/app/directives/person.card.js
@@ -1,10 +1,21 @@
+CardCtrl.$inject = ['$scope', 'PersonService'];
+
+function CardCtrl ($scope, PersonService) {
+  $scope.isDeleting = false;
+  $scope.delete = () => {
+    $scope.isDeleting = true;
+    PersonService.remove($scope.user).then(() => $scope.isDeleting = false);
+  };
+}
+
 function Card() {
   return {
     restrict: 'E',
     scope: {
       user: '='
     },
-    templateUrl: 'templates/card.tpl.html'
+    templateUrl: 'templates/card.tpl.html',
+    controller: CardCtrl
   };
 }
 
